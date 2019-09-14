03c673929f7b2661b4ab9d28f608cf2c80d9588b
techxplorer <flxolola@gmail.com>
2019-09-14 19:10:15 +0300

    added functionality to all functions

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let stepsCounter = 0,
     subtractionResult = 0,
     biggestNumber = 0,
     smallestNumber = 0;
-//Function to verify user input. Preventing all non-integer values
+//Function to verify user input
 const checkUserInput = () => {
     let condition = '';
         if (inputField.value.length < 4) {
@@ -23,14 +23,31 @@ const checkUserInput = () => {
 //Function to create dynamic DOM Nodes
 const createDOM = (condition) => {
     //creating the dynamic DOM nodes
+    let div = document.createElement('div');
+    let appendPosition = document.querySelector('main div');
+    appendPosition.appendChild(div);
     if(condition === 'less-digits') {
-        
+        let p = document.createElement('p');
+        let innerHtml = document.createTextNode(`Oops!<br>Your number should be four-digit!`);
+
+        p.innerHTML = innerHtml;
+        div.appendChild(p);
     }
     else if(condition === 'digits-identical') {
+        let p = document.createElement('p');
+        let innerHtml = document.createTextNode(`Oops!<br>Your number should have at least<br>two none-identical digits!`);
 
+        p.innerHTML = innerHtml;
+        div.appendChild(p);
     }
     else {
+        let p = document.createElement('p');
+        let innerHtml = document.createTextNode(`Yesss!<br>Took your number ${stepsCounter} steps to reach its Kaprekar's constant!<br> Have a look.`);
 
+        p.innerHTML = innerHtml;
+        div.appendChild(p);
+
+        return div;
     }
 }
 
@@ -47,7 +64,7 @@ const kaprekarsAlgo = (numberValue, subtractionResult) => {
         return(b-a);
     });
     //Sort original array in ascending order...
-    let smallestArray = arrayOfNumbers.sort((a, b) => {
+    let smallestArray = arrayOfNumbers.slice().sort((a, b) => {
         return(a-b);
     });
     //use parseint and join to convert sorted arrays into numbers
@@ -61,6 +78,8 @@ const kaprekarsAlgo = (numberValue, subtractionResult) => {
 //Function to append nodes to the DOM
 const appendDOMNodes = () => {
     //clearing the DOM...
+    let nodeToClear = document.querySelector('main div:nth-child(1)');
+    nodeToClear.remove();
 
     let userInput = document.querySelector('input').value;
 
@@ -81,11 +100,11 @@ const appendDOMNodes = () => {
             stepsCounter++;
             //Dom nodes...Create global 
             //create the <p> elements and append to the Div returned by the nodes function.
-
-
+            let p = document.createElement('p');
+            let innerHtml = document.createTextNode(`${biggestNumber} - ${smallestNumber} = ${subtractionResult}`);
+            p.innerHTML = innerHtml;
+            createDOMNodes('all-good').appendChild(p);
         }
-        //Appending the results to the DOM manually
-
     }
 };
 
